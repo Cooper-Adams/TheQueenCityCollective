81bda872052484d34da340af160c760fe8dc9069
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-11-07 00:39:44 -0500

    Provided the query client to the router

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,18 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Router from './components/Router'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router />
+  <QueryClientProvider client={queryClient}>
+    <Router />
+  </QueryClientProvider>
 )
