70b51c734185e962a15fb9715f099a4dfbbb5033
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-11-07 00:35:19 -0500

    Added useQuery to manage data from the netlify function and updated display logic

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -1,17 +1,38 @@
 import Footer from '../components/Footer'
 import Header from '../components/Header'
-import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
+import React, { useState } from 'react'
 import Scroller from '../components/Scroller'
+import SideArticles from '../components/SideArticles'
 import '../styles/Articles.css'
 
 const Articles = () => {
+    const [teamName, setTeamName] = useState('default')
+    const { name } = useParams()
+
+    const {isPending, error, data} = useQuery({
+        queryKey: ['repoData'],
+        queryFn: () => fetch(`/.netlify/functions/getArticles`).then((res) => res.json())
+    })
+
+    //Redirect on query error?
+
     return (
         <>
-            <Header team='default'/>
+            <Header team={teamName}/>
 
-            <Scroller />
+            {isPending &&
+                <h2>Placeholder</h2>
+            }
 
-            <Footer team='default'/>
+            {!isPending && <>
+                <Scroller articles={data.body}/>
+
+                <SideArticles articles={data.body}/>
+            </>}
+
+            <Footer team={teamName}/>
         </>
     )
 }
